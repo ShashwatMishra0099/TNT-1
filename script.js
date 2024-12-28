5907a4b938766289b5fbe9311d6ab67add6a0ef8
Shashwat <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-12-28 07:31:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add click event listeners to navigate to respective pages
-document.getElementById("goIndex").addEventListener("click", function () {
-    window.location.href = "index.html";
-});
-
-document.getElementById("goIndex2").addEventListener("click", function () {
-    window.location.href = "index2.html";
-});
