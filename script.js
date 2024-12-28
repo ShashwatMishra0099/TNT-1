33a2cc44b8608f5ed54fec576c5a4223e93a50c7
Shashwat <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-12-28 06:36:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add click event listeners to navigate to respective pages
+document.getElementById("goIndex").addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+
+document.getElementById("goIndex2").addEventListener("click", function () {
+    window.location.href = "index2.html";
+});
